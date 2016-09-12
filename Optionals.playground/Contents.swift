ca467d021ebc261776d9021957e0fb15bda2510e
Joanna Tzu-Hsuan Huang <joannathhuang@Joannas-MacBook-Pro.local>
2016-09-12 11:23:54 -0400

    Done

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -2,20 +2,74 @@ func brokenFirstItem(items: [String]) -> String {
     return items[0]
 }
 
-var fruits = ["apple", "banana", "grape"]
-brokenFirstItem(fruits)  // Works!
-fruits = []
+//var fruits = ["apple", "banana", "grape"]
+// brokenFirstItem(fruits)  // Works!
+// fruits = []
 // Uncomment the next line to see the problem.
 // You will get an "index out of range" error.
-//brokenFirstItem(fruits)  // Breaks :-(
+// brokenFirstItem(fruits)  // Breaks :-(
 
 
+
+//func firstItem(items: [String]) -> String? {
+//    if items.isEmpty {
+//        return nil
+//    }
+//    return items[0]
+//}
+
+//fruits = ["apple", "banana", "grape"]
+//firstItem(fruits)
+
+//if let fruit = firstItem(fruits) {
+//    print("The first fruit is \(fruit)")
+//} else {
+//    print("There are no fruits!")
+//}
+
+//fruits = []
+//firstItem(fruits)
+
+//if let fruit = firstItem(fruits) {
+//    print("The first fruit is \(fruit)")
+//} else {
+//    print("There are no fruits!")
+//}
+
+
+//func produceFullName(withFirstName firstName: String, middleName: String?, lastName: String) -> String {
+//  var name = firstName
+//     if let middleName = middleName {
+//        name = "\(name) \(middleName)"
+//    }
+//    name = "\(name) \(lastName)"
+//    return name
+//}
+
+//produceFullName(withFirstName: "Jon", middleName: "Bon", lastName: "Jovi")
+//produceFullName(withFirstName: "Mary", middleName: nil, lastName: "Doe")
+
+
+
+
+
+// Joanna's playGround file
+
+//func firstItem(items: [String]) -> String {
+//    return items[0]
+//}
+
+var fruits = ["apple", "banana", "grape"]
+//firstItem(fruits)
+
+//var fruits = []
+//firstItem(fruits)
 
 func firstItem(items: [String]) -> String? {
-    if items.isEmpty {
+    if items.isEmpty{
         return nil
     }
-    return items[0]
+    return items [0]
 }
 
 fruits = ["apple", "banana", "grape"]
@@ -30,14 +84,14 @@ if let fruit = firstItem(fruits) {
 fruits = []
 firstItem(fruits)
 
-if let fruit = firstItem(fruits) {
+if let fruit = firstItem(fruits){
     print("The first fruit is \(fruit)")
 } else {
     print("There are no fruits!")
 }
 
 
-func produceFullName(withFirstName firstName: String, middleName: String?, lastName: String) -> String {
+func produceFullName(withFirstName firstName:String, middleName: String?, lastName:String) -> String {
     var name = firstName
     if let middleName = middleName {
         name = "\(name) \(middleName)"
@@ -48,3 +102,6 @@ func produceFullName(withFirstName firstName: String, middleName: String?, lastN
 
 produceFullName(withFirstName: "Jon", middleName: "Bon", lastName: "Jovi")
 produceFullName(withFirstName: "Mary", middleName: nil, lastName: "Doe")
+produceFullName(withFirstName: "Joanna", middleName: "Tzu-Hsuan", lastName: "Huang")
+
+
